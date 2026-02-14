67789c8681d94936c8b3b2a4af539b44bc68ce87
Ankit chanal <ankitchanal43@mail.com>
2026-02-14 14:47:42 +0530

    empty

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 // 2. CORS: Updated with CORRECT NEW Vercel URL
 app.use(cors({
     origin: [
-        "https://taskflow-app-ten-ashy.vercel.app",  // ✅ NEW URL
+        "https://taaskflow-app.netlify.app",  // ✅ NEW URL
         "http://localhost:5173"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
